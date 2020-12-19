fb8d2395f9b4f02c8f308cb5aa54ef6ac6669139
x48 <x48114@protonmail.com>
2020-12-18 20:07:09 -0600

    fix: don't return "N/A" when no data is available

--- batchCall.js
+++ batchCall.js
@@ -159,7 +159,7 @@ class BatchCall {
               );
             }
             blockResolve({
-              value: data || "N/A",
+              value: data,
               blockNumber,
             });
           };
